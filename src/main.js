c7687dd58a293d2b17b0bd43cf8b63e6aac18a6d
SugarShootingStar <ryu.8224@gmail.com>
2018-09-28 18:43:18 +0900

    feat:実装不可視の世界（ヴァルハラ）に還るために制御したライブラリを模倣し創られた術式兵器をpuppeteerゆえrequest付与cheerioにトランス、それによってレスポンスが爆速になった

--- src/main.js
+++ src/main.js
@@ -1,81 +1,47 @@
-import puppeteer from "puppeteer";
-import Helper from "./nomlishPageHelper";
+const cheerio = require("cheerio");
+let request = require("request");
+request = request.defaults({jar: true});
 
-export default class NomlishTranslate {
-
-  constructor() {
-    this.browser = null;
-    this.page = null;
+const NOMLISH_URL = "https://racing-lagoon.info/nomu/translate.php";
+let params = {
+  method: "POST",
+  url: NOMLISH_URL,
+  form: {
+    level : 2,
+    options : "nochk",
+    transbtn: "翻訳",
   }
+};
 
-  /**
-   * ブラウザの立ち上げ関係処理
-   * 
-   * @export
-   */
-  async init() {
-    this.browser = await puppeteer.launch({
-      // headless: false,
-      args: ['--disable-infobars','--disable-notifications']
+
+/**
+ * テキストをノムリッシュテキストに変換します。
+ *
+ * @export
+ * @param {*} text 変換前テキスト
+ * @param {*} level 翻訳レベル:デフォルトでは2
+ * @returns
+ */
+export function translate(text, level) {
+  return new Promise((resolve, reject) => {
+    params.form.before = text;
+
+    request({url: NOMLISH_URL},function(error,response,body){
+      if (error && response.statusCode == 200) {
+        reject(error);
+      } 
+
+      let $ = cheerio.load(body);
+      params.form.token = $('input[name="token"]').val();
+
+      request.post(params, function(error, response, body){
+        if (error && response.statusCode == 200) {
+          reject(error);
+        }
+        $ = cheerio.load(body);
+        const nomlishText = $('textarea[name="after1"]').text();
+        resolve(nomlishText);
+      })
     });
-    this.page    = await this.browser.newPage();
-    await pageLoadSetting(this.page);
-    await this.page.goto("https://racing-lagoon.info/nomu/translate.php");
-  }
-
-  /**
-   * 日本語をノムリッシュ・テキストに翻訳して返すメイン処理
-   *
-   * @export
-   * @param {*} _text 日本語テキスト
-   * @param {*} _level 翻訳レベル
-   * @returns  ノムリッシュ・テキスト
-   */
-  async translate(_text, _level) {
-    const level = _level || null;
-    // await page.goto("https://racing-lagoon.info/nomu/translate.php");
-    const nomlish = new Helper(_text, level);
-    return translateJapanese(this.page, nomlish);
-  }
-
-  /**
-   * ブラウザを閉じる
-   *
-   * @export
-   */
-  async close() {
-    await this.browser.close();
-  }
-
-}
-
-
-/**
- * ページ読み込み時の設定を行う
- *
- * @param {*} page ノムリッシュ翻訳サイト
- */
-async function pageLoadSetting(page) {
-  // 画像、CSS、フォント、scriptの読み込みをしない指定で高速化
-  await page.setRequestInterception(true);
-  page.on('request', request => {
-    if (['image', 'stylesheet', 'font', 'script'].indexOf(request.resourceType()) !== -1) {
-      request.abort();
-    } else {
-      request.continue();
-    }
   });
-}
-
-/**
- * ノムリッシュテキストを取得するための処理を行う
- *
- * @param {*} page ノムリッシュ翻訳サイト
- * @param {*} nomlish ノムリッシュ翻訳サイトの処理Class
- * @returns ノムリッシュ・テキスト
- */
-async function translateJapanese(page, nomlish) {
-  await nomlish.setFormParamater(page);
-  await nomlish.postForm(page);
-  return nomlish.fetchNomlishText(page);
 }
